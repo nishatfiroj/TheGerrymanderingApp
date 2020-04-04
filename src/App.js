8a7aa5090501076c912d54dcff6e5fddbce70e5a
NishatFiroj <nishat.firoj@gmail.com>
2020-04-04 17:44:47 -0400

    uploading maps and ensuring everything works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 import NavBar from "./Components/navBar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -10,16 +11,19 @@ import History from "./Routes/history";
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Router>
-          <NavBar />
-          <Route path="/history" component={History} />
-          <Route path="/gettingInvolved" component={GettingInvolved} />
-          {/* <Route path="/blank" component={Blank} /> */}
-          <Route path="/caseStudy1" component={CaseStudyOne} />
-          <Route path="/caseStudy2" component={CaseStudyTwo} />
-        </Router>
-      </React.Fragment>
+      <div>
+        <React.Fragment>
+          <Router>
+            <NavBar />
+            <Route path="/history" component={History} />
+            <Route path="/gettingInvolved" component={GettingInvolved} />
+            <Route path="/caseStudy1" component={CaseStudyOne} />
+            <Route path="/caseStudy2" component={CaseStudyTwo} />
+          </Router>
+        </React.Fragment>
+
+
+      </div>
     );
   }
 }
