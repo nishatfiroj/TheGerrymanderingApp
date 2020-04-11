bbf48141491f883ca9b242fa7ff4a14202a42e1c
byuan45 <44277100+byuan45@users.noreply.github.com>
2020-04-11 15:08:00 -0400

    added in caseStudy3, updated caseStudy1 and 2 and getting involved (#4)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import GettingInvolved from "./Routes/gettingInvolved";
 import CaseStudyOne from "./Routes/caseStudyOne";
 import CaseStudyTwo from "./Routes/caseStudyTwo";
 import CaseStudyThree from "./Routes/caseStudyThree";
+<<<<<<< HEAD
+=======
 import Home from "./Routes/home"
+>>>>>>> dafd7f17c7d95206d348801ed8b20ddc703d1a93
 
 import History from "./Routes/history";
 
@@ -25,8 +28,13 @@ class App extends Component {
             <Route path="/caseStudy2" component={CaseStudyTwo} />
             <Route path="/caseStudy3" component={CaseStudyThree} />
           </Router>
+<<<<<<< HEAD
+        </React.Fragment>
+      </div>
+=======
 
         </React.Fragment>
+>>>>>>> dafd7f17c7d95206d348801ed8b20ddc703d1a93
     );
   }
 }
